5d9e3e68d63708cc760903e8056898c79cc2d4c5
koubaahm <ahmed.koubaa@univ-rouen.fr>
2024-12-06 11:17:10 +0100

    initialisation projet

---
+++ src/main/java/univ_rouen/fr/Insta_lite/InstaLiteApplication.java
@@ -0,0 +1,13 @@
+package univ_rouen.fr.Insta_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/univ_rouen/fr/Insta_lite/InstaLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package univ_rouen.fr.Insta_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
